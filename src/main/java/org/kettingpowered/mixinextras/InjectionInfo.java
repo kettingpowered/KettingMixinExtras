907b9a3582abc9df91e4f026c9ff0396b7f4a25e
JustRed23 <manuel.bormans@gmail.com>
2026-05-09 11:33:38 +0200

    Add annotation node to InjectionInfo record, only process annotationValues when it is needed

--- src/main/java/org/kettingpowered/mixinextras/InjectionInfo.java
+++ src/main/java/org/kettingpowered/mixinextras/InjectionInfo.java
@@ -1,9 +1,28 @@
 package org.kettingpowered.mixinextras;
 
 import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-record InjectionInfo(ClassNode targetClass, IMixinInfo mixinInfo, @NotNull Map<String, Object> annotationValues) {}
+record InjectionInfo(ClassNode targetClass, IMixinInfo mixinInfo, AnnotationNode annotation) {
+
+    @NotNull Map<String, Object> annotationValues() {
+        if (annotation == null || annotation.values == null || annotation.values.isEmpty()) return Collections.emptyMap();
+        Map<String, Object> annotationValues = new HashMap<>(annotation.values.size() / 2);
+        for (int i = 0; i + 1 < annotation.values.size(); i += 2) {
+            Object key = annotation.values.get(i);
+            Object value = annotation.values.get(i + 1);
+
+            if (key instanceof String stringKey)
+                annotationValues.put(stringKey, value);
+            else
+                KettingMixinPlugin.LOGGER.warn("Annotation key at index {} is not a String in {}", i, annotation.desc);
+        }
+        return Map.copyOf(annotationValues);
+    }
+}
--- src/main/java/org/kettingpowered/mixinextras/TransformerRegistry.java
+++ src/main/java/org/kettingpowered/mixinextras/TransformerRegistry.java
@@ -1,8 +1,5 @@
 package org.kettingpowered.mixinextras;
 
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
@@ -12,7 +9,6 @@ import org.spongepowered.asm.util.Annotations;
 
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 final class TransformerRegistry {
@@ -34,40 +30,22 @@ final class TransformerRegistry {
     private int applyMethod(ClassNode targetClass, IMixinInfo mixinInfo, MethodNode method) {
         if (method == null || method.invisibleAnnotations == null) return 0;
         return methodTransformers.entrySet().stream().mapToInt(kv -> {
-            var ann = kv.getKey();
-            var transformer = kv.getValue();
-            AnnotationNode annotationNode = Annotations.getInvisible(method, ann);
+            AnnotationNode annotationNode = Annotations.getInvisible(method, kv.getKey());
             if (annotationNode == null) return 0;
             KettingMixinPlugin.log("Applying transformation to: {}:{}", targetClass.name, method.name);
-            var info = new InjectionInfo(targetClass, mixinInfo, listToMap(ann, annotationNode.values));
-            return transformer.transform(info, method);
+            InjectionInfo info = new InjectionInfo(targetClass, mixinInfo, annotationNode);
+            return kv.getValue().transform(info, method);
         }).sum();
     }
 
     private int applyField(ClassNode targetClass, IMixinInfo mixinInfo, FieldNode field) {
         if (field == null || field.invisibleAnnotations == null) return 0;
         return fieldTransformers.entrySet().stream().mapToInt(kv -> {
-            var ann = kv.getKey();
-            var transformer = kv.getValue();
-            AnnotationNode annotationNode = Annotations.getInvisible(field, ann);
+            AnnotationNode annotationNode = Annotations.getInvisible(field, kv.getKey());
             if (annotationNode == null) return 0;
             KettingMixinPlugin.log("Applying transformation to field: {}.{}", targetClass.name, field.name);
-            var info = new InjectionInfo(targetClass, mixinInfo, listToMap(ann, annotationNode.values));
-            return transformer.transform(info, field);
+            InjectionInfo info = new InjectionInfo(targetClass, mixinInfo, annotationNode);
+            return kv.getValue().transform(info, field);
         }).sum();
-    }
-
-    @Contract("_, _ -> !null")
-    private static @NotNull Map<String, Object> listToMap(@NotNull Class<? extends Annotation> clazz, @Nullable List<Object> object){
-        var map = new HashMap<String, Object>();
-        if (object == null) return map;
-        var iter = object.listIterator();
-        while (iter.hasNext()) {
-            Object key = iter.next();
-            Object value = iter.hasNext() ? iter.next() : null;
-            if (key instanceof String stringKey) map.put(stringKey, value);
-            else KettingMixinPlugin.log("The key of Annotation {} is not a String (is instead {})? Ignoring value", clazz.getCanonicalName(), key.getClass().getCanonicalName());
-        }
-        return map;
     }
 }
